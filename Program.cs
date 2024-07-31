69c8777250f5aed2a9a5d48262ecfbee5dc2518a
yuri <yurifuchidaalves@gmail.com>
2024-07-31 02:56:13 -0300

    quickstart

--- Program.cs
+++ Program.cs
@@ -1,55 +1,18 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Threading.Tasks;
+using Telegram.Bot;
 
-public class Program
-{
-    public static async Task Main(string[] args)
-    {
-        await Demo();
-        Console.ReadLine();
-    }
+public class Program {
+  static async Task Main() {
+    await MyBot.Teste();
+  }
+}
 
-    public static async Task Demo()
-    {
-        var watch = new Stopwatch();
-        watch.Start();
-
-
-        // Aguarde todas as tarefas concluírem
-        await StartSchoolAssembly();
-        await TeachClass12();
-        await TeachClass11();
-
-        watch.Stop();
-        Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
-    }
-
-    public static async Task StartSchoolAssembly()
-    {
-        await Task.Run(() =>
-        {
-          Func<int, int, int> add = (a, b) => a + b;
-            Task.Delay(8000).Wait(); // Task.Delay é preferível a Thread.Sleep
-            Console.WriteLine("School Started");
-        });
-    }
-
-    public static async Task TeachClass12()
-    {
-        await Task.Run(() =>
-        {
-            Task.Delay(3000).Wait(); // Task.Delay é preferível a Thread.Sleep
-            Console.WriteLine("Taught class 12");
-        });
-    }
-
-    public static async Task TeachClass11()
-    {
-        await Task.Run(() =>
-        {
-            Task.Delay(2000).Wait(); // Task.Delay é preferível a Thread.Sleep
-            Console.WriteLine("Taught class 11");
-        });
-    }
+class MyBot {
+  public static async Task Teste() {
+    const string botToken = "6906889561:AAHpmGcdXxPZOLbkEOLC67STCICwDxymkTs";
+    TelegramBotClient bot = new TelegramBotClient(botToken);
+    Telegram.Bot.Types.User me = await bot.GetMeAsync();
+    Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+  }
 }
